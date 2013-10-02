c11503415c2f290939db309d84ea095367201d35
Federico Fissore <federico@fissore.org>
2013-10-02 10:45:46 +0200

    PHP class: reviewed formatting (removed trailing spaces, replaced 4 spaces with tabs)

--- bridge/php/bridgeclient.class.php
+++ bridge/php/bridgeclient.class.php
@@ -21,55 +21,56 @@ You have only two methods: get and put (see example.php)
 error_reporting(E_ERROR);
 
 class bridgeclient {
-        
-        private $service_port = 5700;
-        private $address = "127.0.0.1";
-        private $socket;
-        
-        private function connect() {
-            ($this->socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) 
-                || die("socket_create() failed: " . socket_strerror(socket_last_error()) . "\n");
-            socket_set_option($this->socket,SOL_SOCKET, SO_RCVTIMEO, array("sec"=>3, "usec"=>0));
-            socket_connect($this->socket, $this->address, $this->service_port) 
-                || die("socket_connect() failed: " . socket_strerror(socket_last_error($this->socket)) . "\n");            
-        }
-        
-        private function disconnect() {            
-            socket_close($this->socket);            
-        }        
 
-        private function sendcommand($command,$key,$value="") {
-            $jsonreceive = "";
-            $obraces=0;
-            $cbraces=0;
+	private $service_port = 5700;
+	private $address = "127.0.0.1";
+	private $socket;
+	
+	private function connect() {
+		($this->socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) 
+		|| die("socket_create() failed: " . socket_strerror(socket_last_error()) . "\n");
+		socket_set_option($this->socket,SOL_SOCKET, SO_RCVTIMEO, array("sec"=>3, "usec"=>0));
+		socket_connect($this->socket, $this->address, $this->service_port) 
+		|| die("socket_connect() failed: " . socket_strerror(socket_last_error($this->socket)) . "\n");
+	}
+	
+	private function disconnect() {
+		socket_close($this->socket);
+	}
 
-            $this->connect();            
-            
-            $jsonsend = '{"command":"'.$command.'","key":"'.$key.'","value":"'.$value.'"}';
-            socket_write($this->socket, $jsonsend, strlen($jsonsend));
-        
-            do {
-                socket_recv($this->socket, $buffer, 1,0);                 
-                $jsonreceive.=$buffer;
-                if($buffer == "{") $obraces++;
-                if($buffer == "}") $cbraces++;
-            } while ($obraces != $cbraces);
-            
-            $this->disconnect();
-            
-            $jsonarray=json_decode($jsonreceive);
-            if ($jsonarray->{'value'} == NULL) $jsonarray->{'value'}="None";
-            
-            return $jsonarray->{'value'};            
-        }
-        
-        public function get($key) {                
-            return $this->sendcommand("get",$key);
-        }
-        
-        public function put($key,$value) {
-            return $this->sendcommand("put",$key,$value);
-        }
+	private function sendcommand($command,$key,$value="") {
+		$jsonreceive = "";
+		$obraces=0;
+		$cbraces=0;
+
+		$this->connect();
+		
+		$jsonsend = '{"command":"'.$command.'","key":"'.$key.'","value":"'.$value.'"}';
+		socket_write($this->socket, $jsonsend, strlen($jsonsend));
+	
+		do {
+		socket_recv($this->socket, $buffer, 1,0);
+		$jsonreceive.=$buffer;
+		if($buffer == "{") $obraces++;
+		if($buffer == "}") $cbraces++;
+		} while ($obraces != $cbraces);
+		
+		$this->disconnect();
+		
+		$jsonarray=json_decode($jsonreceive);
+		if ($jsonarray->{'value'} == NULL) $jsonarray->{'value'}="None";
+		
+		return $jsonarray->{'value'};
+	}
+	
+	public function get($key) {
+		return $this->sendcommand("get",$key);
+	}
+	
+	public function put($key,$value) {
+		return $this->sendcommand("put",$key,$value);
+	}
 
 }
 ?>
+
