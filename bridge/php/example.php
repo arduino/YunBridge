a1d7ec18ff30c89a5563a63311dafb4ae9ecedeb
5a2v0 <saving@hotmail.it>
2014-05-14 16:44:15 +0200

    Update example.php
    Added example string to explain new getall() function

--- bridge/php/example.php
+++ bridge/php/example.php
@@ -4,7 +4,12 @@ require ("bridgeclient.class.php");
 $client = new bridgeclient();
 
 $client->put("D13","Test");
-$test= $client->get("D13");
+
+$test = $client->get("D13");
 echo $test;
+
+$all = $client->getall();
+$all = json_decode($all, true);
+print_r($all);
 ?>
 
